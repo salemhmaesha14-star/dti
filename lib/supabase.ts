136a1425d39c08a545f4f9d9d5953d7225a12953
LCT <lct@example.com>
2026-09-19 01:03:30 +0300

    fix supabase env vars

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://abqfziattkyzosemffgy.supabase.co';
-const supabaseAnonKey = 'sb_publishable_f5mhRbwyVFqZ4DgdDUzuRQ_cTnTXgRv';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
