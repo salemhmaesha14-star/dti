02b77b66f773835285d6f1c3d00a054c2def0e60
LCT <lct@example.com>
2026-09-18 21:29:44 +0300

    fix netlify config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true,
   reactStrictMode: true,
   images: {
     unoptimized: true,
-  },
-  experimental: {
-    typedRoutes: false,
   },
 };
 
